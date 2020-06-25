4effbcf0a9a15cb376aaa995cf0359fa88aa2784
vladeta97 <vladeta.manic@elfak.rs>
2020-06-26 01:16:24 +0200

    ---

--- iot-services/services/fosteractuator.service.js
+++ iot-services/services/fosteractuator.service.js
@@ -1,31 +1,27 @@
 "use strict";
 
 module.exports = {
-	name: "fosteractuator",
-	actions: {
-		set: {
-			params: {
-				offset: { type: "number" },
-				sensorId: { type: "number" }
-			},
-			async handler(ctx) {
-				console.log(ctx.params);
-                this.broker.emit(`temperature.set.${ctx.params.sensorId}`, ctx.params);
-                return 'Success setting temperature!';
-			}
-        }
-	},
-	events:{
-		"actuator.foster": {
-			group: "other",
-			handler(payload) {
-			    console.log(
-					'Foster stanica prima komandu sa command servisa...',
-					
-				);
-				console.log("Foster actuatos izvrsava komandu " + payload.command+" ! ")
-				
-			}
-		},
-	}
+  name: "fosteractuator",
+  actions: {
+    set: {
+      params: {
+        temp: { type: "number" },
+      },
+      async handler(ctx) {
+        console.log(ctx.params);
+        return "Recived data from command";
+      },
+    },
+  },
+  events: {
+    "actuator.foster": {
+      group: "other",
+      handler(payload) {
+        console.log("Foster stanica prima komandu sa command servisa...");
+        console.log(
+          "Foster actuatos izvrsava komandu " + payload.command + " ! "
+        );
+      },
+    },
+  },
 };
